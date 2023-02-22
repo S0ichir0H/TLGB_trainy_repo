4a9a679d9ff7c418b18a6d42067b74f9e5b9b8ba
S0ichir0H <dark-silence@ya.ru>
2023-02-22 18:43:43 +0300

    распределение кода по отдельным модулям

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,49 +1,11 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-import os
-
-
-bot = Bot(token=os.getenv('TOKEN'))
-dp = Dispatcher(bot)
+from create_bot import dp
+from handlers import client, admin, other
 
 async def on_startup(_):
 	print("=============================================================\n\nБот вышел в онлайн")
 
-########## КЛИЕНТСКАЯ ЧАСТЬ ##########
-@dp.message_handler(commands=['start', 'help'])
-async def command_start(message: types.Message):
-	try:
-		await bot.send_message(message.from_user.id, 'Приятного аппетита!')
-		await message.delete()
-	except:
-		await message.reply("Общение с ботом через ЛС, напишите ему:\nhttps://t.me/BonBonStore_bot")
-
-@dp.message_handler(commands=['Режим_работы'])
-async def pizza_open_command(message: types.Message):
-	await bot.send_message(message.from_user.id, 'Вс-Чт с 09:00 до 20:00, Пт-Сб с 10:00 до 23:00')
-
-@dp.message_handler(commands=['Расположение'])
-async def pizza_open_command(message: types.Message):
-	await bot.send_message(message.from_user.id, 'ул. Колбасная, 15')
-
-# @dp.message_handler(commands=['Меню'])
-# async def pizza_menu_command(message: types.Message):
-# 	for ret in cur.execute('SELECT * FROM menu').fetchall():
-# 		await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦенв {ret[-1]}')
-
-
-########## АДМИНСКАЯ ЧАСТЬ ##########
-
-########## ОБЩАЯ ЧАСТЬ ##########
-
-@dp.message_handler()
-async def echo_send(message: types.Message):
-	if message.text == 'Привет':
-		await message.answer('И тебе привет!!')
-	# await message.reply(message.text)
-	# await bot.send_message(message.from_user.id, message.text)
-
-
+client.register_handler_client(dp)
+other.register_handler_other(dp)
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
---
+++ handlers/other.py
@@ -0,0 +1,25 @@
+from aiogram import types, Dispatcher
+import json, string
+from create_bot import dp
+
+########## ОБЩАЯ ЧАСТЬ ##########
+
+# фильтр обновляется только при запуске бота
+jsload = set(json.load(open('anti_mat.json')))
+# @dp.message_handler()
+
+
+async def echo_send(message: types.Message):
+	if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(' ')}\
+		.intersection(jsload):
+		await message.reply('Маты запрещены')
+		await message.delete()
+	elif message.text.lower() in ["хай", "привет"]:
+		await message.answer('И тебе привет!!')
+	# await message.reply(message.text)
+	# await bot.send_message(message.from_user.id, message.text)
+
+
+def register_handler_other(dp : Dispatcher):
+	dp.register_message_handler(echo_send)
+
